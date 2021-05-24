7b4dcfa8c53a9a7c618df571ad25dc1c86cb9059
piyush <piyushchaurasia82@gmail.com>
2021-05-24 11:07:35 +0530

    new design add

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ $(window).on("scroll", function() {
         $("header").addClass("header");
         $("header ul li").addClass("color_black");
         $(".globbtn_res").addClass("border_black");
+        // $(".logo_fir").css("display","none");
 
     } else {
         //remove the background property so it comes transparent again (defined in your css)
        $("header").removeClass("header");
        $("header ul li").removeClass("color_black");
        $(".globbtn_res").removeClass("border_black");
+       // $(".logo_sec").css("display","block");
 
 
     }
